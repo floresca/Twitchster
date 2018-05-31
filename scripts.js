ac1315b2f2ad135a0406e65b502bac392410fd8a
Andres <floresc.andres@intracitygeeks.org>
2018-05-31 18:54:59 -0400

    API address calls for offline and channel info now return objects

--- scripts.js
+++ scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-    var whoCalls = "Name of channel";
-    var twitchCall = "https://wind-bow.glitch.me/twitch-api/streams/" + whoCalls;
+    var whoCalls = ["freecodecamp", "test_channel", "twitchpresents"];
+    var twitchTRUE = "https://wind-bow.glitch.me/twitch-api/streams/";
+    var twitchINFO = "https://wind-bow.glitch.me/twitch-api/channel/";
+    
+//Loop through the defaults and create a unique link for each
+    for(var i = 0; i < whoCalls.length; i++){
+        var twitchUNIQUE;
+        twitchUNIQUE = twitchTRUE + whoCalls[i];
 
+//call the API to confirm if the channel is offline      
+        $.getJSON(twitchUNIQUE, isItOnline);
+        
+        
+        function isItOnline(maybeItIS){
+            if(maybeItIS.stream == "null"){
+                stream status offline
+            } 
+        }
+//loop assigns new links for channel info
+        var twitchGET;
+        twitchGET = twitchINFO + whoCalls[i];
+        
+//call the API for information
+
+        $.getJSON(twitchGET,twitchPUSH)
+        
+        function twitchPUSH(pushedInfo){
+            Push info gotten from the channel
+        }
+    }
 });
