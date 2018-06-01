29cf439a4134c8e0d2db04cff26893a5edd3a170
Andres <floresc.andres@intracitygeeks.org>
2018-05-31 20:45:11 -0400

    JavaScript now pulls API info and appends to HTML

--- scripts.js
+++ scripts.js
@@ -1,32 +1,53 @@
 $(document).ready(function() {
     var whoCalls = ["freecodecamp", "test_channel", "twitchpresents"];
     var twitchTRUE = "https://wind-bow.glitch.me/twitch-api/streams/";
-    var twitchINFO = "https://wind-bow.glitch.me/twitch-api/channel/";
+    var twitchINFO = "https://wind-bow.glitch.me/twitch-api/channels/";
     
 //Loop through the defaults and create a unique link for each
     for(var i = 0; i < whoCalls.length; i++){
-        var twitchUNIQUE;
-        twitchUNIQUE = twitchTRUE + whoCalls[i];
-
-//call the API to confirm if the channel is offline      
-        $.getJSON(twitchUNIQUE, isItOnline);
-        
-        
-        function isItOnline(maybeItIS){
-            if(maybeItIS.stream == "null"){
-                stream status offline
-            } 
-        }
-//loop assigns new links for channel info
         var twitchGET;
         twitchGET = twitchINFO + whoCalls[i];
-        
-//call the API for information
 
+//Call the API and get information from it
         $.getJSON(twitchGET,twitchPUSH)
         
         function twitchPUSH(pushedInfo){
-            Push info gotten from the channel
+            var Cicon = pushedInfo.logo;
+            var CName = pushedInfo.display_name;
+            var CShowing = pushedInfo.status;
+            var CLink = pushedInfo.url;
+            
+//Create an LI element with span children to share the same space
+            var list =  "<a href='"+CLink+"'>" +
+                            "<li class='ChannelItem'>" +
+                                "<span class='channel_icon'>" +
+                                    "<img src='"+Cicon+"' class='icon'>" +
+                                "</span>" +
+                                "<span class='channel_name'>"+CName+"</span>" +
+                                "<span class='channel_showing'>"+CShowing+"</span>" +
+                                // "<span class='channel_remove glyphicon glyphicon-remove'></span>" +
+                            "</li>" +
+                        "</a>";
+
+//Append our new LI into the UL element for every channel
+            $("ul").append(list);
         }
     }
 });
+
+
+//call the API to confirm if the channel is offline    
+
+        // var twitchUNIQUE;
+        // twitchUNIQUE = twitchTRUE + whoCalls[i];
+        
+        // $.getJSON(twitchUNIQUE, isItOnline);
+        
+        
+        // function isItOnline(maybeItIS){
+        //     if(maybeItIS.stream == null){
+        //         $("li").css({"background-color": "#d9d9d9"});
+        //     } else {
+        //         $("li").css({"background-color": "#99ff66"});
+        //     }
+        // }
