0fb4035fcaaf598b4902990072fe066aa515d228
Andres <floresc.andres@intracitygeeks.org>
2018-06-01 00:17:47 -0400

    if else statement for online/offline

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     var whoCalls = ["freecodecamp", "test_channel", "twitchpresents"];
-    var twitchTRUE = "https://wind-bow.glitch.me/twitch-api/streams/";
     var twitchINFO = "https://wind-bow.glitch.me/twitch-api/channels/";
+    var twitchTRUE = "https://wind-bow.glitch.me/twitch-api/streams/";
+    var twitchGET;
+    var AreWeLive;
     
 //Loop through the defaults and create a unique link for each
     for(var i = 0; i < whoCalls.length; i++){
-        var twitchGET;
         twitchGET = twitchINFO + whoCalls[i];
-
+        
 //Call the API and get information from it
         $.getJSON(twitchGET,twitchPUSH)
         
@@ -31,6 +32,21 @@ $(document).ready(function() {
 
 //Append our new LI into the UL element for every channel
             $("ul").append(list);
+            
+        }
+        
+
+//This is the online vs offline   
+        AreWeLive = twitchTRUE + whoCalls[i];
+        console.log(AreWeLive);
+        $.getJSON(AreWeLive, twitchON);
+            
+        function twitchON(twitchLIVE){
+            if(twitchLIVE.stream == null){
+                $("li").css({"background-color": "#d9d9d9"});
+            } else {
+                $("li").css({"background-color": "#99ff66"});
+            }
         }
     }
 });
